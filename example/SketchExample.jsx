6a87834fc99ea1b125cd0e3a4cefc0624ef1617e
Ubuntu <ubuntu@ubuntu2104.linuxvmimages.local>
2021-10-03 02:58:21 -0400

    front end push

--- example/SketchExample.jsx
+++ example/SketchExample.jsx
@@ -6,6 +6,19 @@ const wsClient = IO(`ws://127.0.0.1:12346`);
 
 export default class SketchExample extends Component
 {
+
+//look up tailwindcss guide to intergrate with
+
+ saveCanvas() {
+    var canvas = document.getElementById("canvas").firstElementChild;
+    var ctx = canvas.getContext("2d");
+    var data = canvas.toDataURL();
+    
+  console.log(data);
+    
+    console.log('Saved!');
+  }
+  
   socket = null;
 
   constructor(props) {
@@ -26,66 +39,36 @@ export default class SketchExample extends Component
   }
 
   render() {
-    const { tool, size, color, fill, fillColor, items } = this.state;
+    const {size} = this.state;
     return (
       <div>
-        <h1>React SketchPad</h1>
-        <div style={{float:'left', marginRight:20}}>
+         <form>
+     <p> <label>
+    First Name:  <p>  <input type="text" name="First name" /> </p>
+  </label></p>
+  <p>  <label>
+    Last Name:  <p>  <input type="text" name="Last name" /> </p>
+  </label></p>
+<p><label>
+Date:     <p>    <input type="text" name="Date" /> </p>
+</label>
+</p>
+</form>     
+        <div style={{float:'left', marginRight:20}} id="canvas">
           <SketchPad
             width={500}
-            height={500}
+            height={150}
             animate={true}
             size={size}
-            color={color}
-            fillColor={fill ? fillColor : ''}
-            items={items}
-            tool={tool}
-            onCompleteItem={(i) => wsClient.emit('addItem', i)}
+            onCompleteItem={(i) => wsClient.emit('addItem', i)}            
           />
         </div>
-        <div style={{float:'left'}}>
-          <div className="tools" style={{marginBottom:20}}>
-            <button
-              style={tool == TOOL_PENCIL ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_PENCIL  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_PENCIL})}
-            >Pencil</button>
-            <button
-              style={tool == TOOL_LINE ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_LINE  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_LINE})}
-            >Line</button>
-            <button
-              style={tool == TOOL_ELLIPSE ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_ELLIPSE  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_ELLIPSE})}
-            >Ellipse</button>
-            <button
-              style={tool == TOOL_RECTANGLE ? {fontWeight:'bold'} : undefined}
-              className={tool == TOOL_RECTANGLE  ? 'item-active' : 'item'}
-              onClick={() => this.setState({tool:TOOL_RECTANGLE})}
-            >Rectangle</button>
-          </div>
-          <div className="options" style={{marginBottom:20}}>
-            <label htmlFor="">size: </label>
-            <input min="1" max="20" type="range" value={size} onChange={(e) => this.setState({size: parseInt(e.target.value)})} />
-          </div>
-          <div className="options" style={{marginBottom:20}}>
-            <label htmlFor="">color: </label>
-            <input type="color" value={color} onChange={(e) => this.setState({color: e.target.value})} />
-          </div>
-          {(this.state.tool == TOOL_ELLIPSE || this.state.tool == TOOL_RECTANGLE) ?
-            <div>
-              <label htmlFor="">fill in:</label>
-              <input type="checkbox" value={fill} style={{margin:'0 8'}}
-                     onChange={(e) => this.setState({fill: e.target.checked})} />
-              {fill ? <span>
-                  <label htmlFor="">with color:</label>
-                  <input type="color" value={fillColor} onChange={(e) => this.setState({fillColor: e.target.value})} />
-                </span> : ''}
-            </div> : ''}
-        </div>
-      </div>
+        <div style={{float:'left'}}>         
+            <button onClick={()=> {this.saveCanvas()}}>
+  Submit Signature
+</button>         
+        </div>       
+      </div>     
     );
   }
 }
